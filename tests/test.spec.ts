ec2f983b0f7c06c79cd81767668ee54bc04a1aea
burakkaanerce <burak.ercedogan@gmail.com>
2020-11-29 05:40:47 +0300

    added new test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 
-import { app } from '../src/server';
+import app from '../src/server';
 
 describe('Test MainRoute', () => {
   it('/api/v1/algorithms/find-distance"', async (done) => {
@@ -10,6 +10,17 @@ describe('Test MainRoute', () => {
 
     expect(result.status).toBe(200);
     expect(result.body.result).toBe(2);
+
+    console.log("timer: ", result.body.timer);
+    done();
+  });
+  it('/api/v1/algorithms/spell-check"', async (done) => {
+    const result = await request(app.app)
+      .post('/api/v1/algorithms/spell-check')
+      .send({ word: 'abandonment' });
+
+    expect(result.status).toBe(200);
+    expect(result.body.isIncluded).toBe(true);
 
     console.log("timer: ", result.body.timer);
     done();
